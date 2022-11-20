43166b605cb0681dfcc1ef2d27e3765356150838
KeYunTinG <zaac2240632@yahoo.com.tw>
2022-11-20 23:30:16 +0800

    feat bodyparser and mongoose

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })//避開警告訊息
+// MongoDB-connection status
+const db = mongoose.connection
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/', (req, res) => {
-    res.render('shortenUrl')
+    const originalURL = req.body.original_URL
+    res.render('shortenUrl', { originalURL })
 })
 
 app.listen(PORT, () => {
